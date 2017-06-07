1bbd5b2cfe20d3c379281a9999c42d67308db50a
bigrocs <bigrocs@qq.com>
2017-06-07 15:34:34 +0800

    对menus数据 进行树型数据结构处理

--- src/Builder/Main.php
+++ src/Builder/Main.php
@@ -10,6 +10,7 @@ class Main
     private $config;
     private $apiUrl;
     private $topNavs;
+    private $menus;
     /**
      * Create a new Skeleton Instance
      */
@@ -50,7 +51,7 @@ class Main
      * @param    [type]                   $topNav [顶部导航配置数据]
      * @return   [type]                           [description]
      */
-    public function topNavList($topNav){
+    public function addTopNavList($topNav){
         return $this->topNavs['list'][$topNav['name']] = $topNav;
     }
     /**
@@ -61,6 +62,50 @@ class Main
         $topNavsList = collect($this->topNavs['list'])->sortBy('sort');
         $this->topNavs['list'] = $topNavsList;
         return $this->topNavs;
+    }
+    /**
+     * [getMenus 获取处理后的菜单数据]
+     * @return [type] [description]
+     */
+    public function getMenus(){
+      return $this->menus;
+    }
+    /**
+     * [setMenus 批量设置导航菜单]
+     * @param    [type]                   $menus [description]
+     */
+    public function setMenus($menus){
+        $menus = $menus->map(function ($menu) {
+            $menu->path = $menu->value;
+            $menu->apiUrl = $menu->api_url;
+            return $menu;
+        })->filter(function ($menu, $key) use($menus) {
+            if ($menu->pid == 0) {
+                $menu = $this->getSubMenus($menu,$menus);
+            }
+            return $menu->pid == 0;
+        });
+        return $this->menus  = $menus;
+    }
+
+    /**
+     * [getSubMenus 设置子导航(可无限级设置)]
+     * @param    [type]                   $menu  [过滤后顶级菜单]
+     * @param    [type]                   $menus [菜单原始数据]
+     * @return   [type]                          [description]
+     */
+    public function getSubMenus($menu,$menus){
+        $id = $menu->id;
+        $subMenus = $menus->filter(function ($menu, $key) use($id,$menus) {
+            if ($menu->pid == $id) {
+                $menu = $this->getSubMenus($menu,$menus);
+            }
+            return $menu->pid == $id;
+        });
+        if (!$subMenus->isEmpty()) {
+            $menu->subMenus = $subMenus;
+        }
+        return $menu;
     }
     /**
      * [config 前端主要配置参数]
