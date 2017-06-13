4c50832ac918f74f5a919c4e3d0d5b5becca0969
bigrocs <bigrocs@qq.com>
2017-06-13 14:45:24 +0800

    增加 tabsGroup  分组字段名设定。允许后端自定义

--- src/Builder/Form.php
+++ src/Builder/Form.php
@@ -8,6 +8,8 @@ class Form
   private $data;
   private $apiUrl;
   private $config;
+  private $tabs;
+  private $tabsGroup;
   private $response;
 
   public function __construct()
@@ -50,16 +52,26 @@ class Form
       return $this;
   }
   /**
+   * [tabsGroup tabs分组字段]
+   * @param  [type] $name [字段名]
+   * @return [type]       [description]
+   */
+  public function tabsGroup($name){
+      $this->tabsGroup   = $name;
+      return $this;
+  }
+  /**
    * [response 数据处理返回]
    * @return   [type]                   [输出后的form数据]
    */
   public function response()
   {
-      $this->response['type'] = $this->type;
-      $this->response['data'] = $this->data;
-      $this->response['tabs']   = $this->tabs;
-      $this->response['apiUrl'] = $this->apiUrl;
-      $this->response['config'] = $this->config;
+      $this->response['type']       = $this->type;
+      $this->response['data']       = $this->data;
+      $this->response['tabs']       = $this->tabs;
+      $this->response['tabsGroup']  = $this->tabsGroup;
+      $this->response['apiUrl']     = $this->apiUrl;
+      $this->response['config']     = $this->config;
       return $this->response;
   }
 }
