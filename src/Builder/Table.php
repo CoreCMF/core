e2df6a660430433d27d1a8b051bb2c341f6738c9
bigrocs <bigrocs@qq.com>
2017-06-19 17:29:51 +0800

    add table data  column topButton rightButton response

--- src/Builder/Table.php
+++ src/Builder/Table.php
@@ -4,11 +4,67 @@ namespace CoreCMF\core\Builder;
 
 class Table
 {
+  private $type = 'table';
+  private $column;
+  private $topButton;
+  private $rightButton;
+  private $response;
   /**
    * Create a new Skeleton Instance
    */
   public function __construct()
   {
 
+  }
+  /**
+   * [tabs description]
+   */
+  public function tabs($tabs){
+      $this->tabs   = $tabs;
+      return $this;
+  }
+  /**
+   * [tabsGroup tabs分组字段]
+   */
+  public function tabsGroup($name){
+      $this->tabsGroup   = $name;
+      return $this;
+  }
+  /**
+   * [data 设置数据源]
+   * @param  [type] $data [数据对象]
+   */
+  public function data($data){
+      $this->data = $data;
+      return $this;
+  }
+  public function column($column){
+      $this->column[] = $column;   //设置 tableColumn 表格柱
+      return $this;
+  }
+  public function topButton($topButton){
+      $this->topButton[]   = $topButton;   //设置 table rightButton 表格右侧按钮
+      return $this;
+  }
+  public function rightButton($rightButton){
+      $this->rightButton[]   = $rightButton;   //设置 table rightButton 表格右侧按钮
+      return $this;
+  }
+  /**
+   * [response 数据处理返回]
+   * @return   [type]                   [输出后的form数据]
+   */
+  public function response()
+  {
+      $this->response['type']       = $this->type;
+      $this->response['tabs']       = $this->tabs;
+      $this->response['tabsGroup']  = $this->tabsGroup;
+      $this->response['data']       = $this->data;
+      $this->response['column']     = $this->column;
+      $this->response['topButton']  = $this->topButton;
+      $this->response['rightButton']= $this->rightButton;
+      // $this->response['apiUrl']     = $this->apiUrl;
+      // $this->response['config']     = $this->config;
+      return $this->response;
   }
 }
