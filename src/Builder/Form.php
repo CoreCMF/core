a927c09152e9bf41a7257d8d4bf8d046cbb3bc53
bigrocs <bigrocs@qq.com>
2017-06-22 09:31:53 +0800

    add itemType item itemData

--- src/Builder/Form.php
+++ src/Builder/Form.php
@@ -11,12 +11,43 @@ class Form
   private $tabs;
   private $tabsGroup;
   private $response;
+  public  $itemType = [
+          'hidden'     => '隐藏',
+          'static'     => '不可修改文本',
+          'number'     => '数字',
+          'price'      => '价格',
+          'text'       => '单行文本',
+          'textarea'   => '多行文本',
+          'array'      => '数组',
+          'password'   => '密码',
+          'radio'      => '单选按钮',
+          'checkbox'   => '复选框',
+          'select'     => '下拉框',
+          'icon'       => '字体图标',
+          'date'       => '日期',
+          'datetime'   => '时间',
+          'picture'    => '单张图片',
+          'pictures'   => '多张图片',
+          'file'       => '单个文件',
+          'files'      => '多个文件',
+          'kindeditor' => 'HTML编辑器 kindeditor',
+          'editormd'   => 'Markdown编辑器 editormd',
+          'tags'       => '标签',
+          'board  '    => '拖动排序',
+  ];
 
   public function __construct()
   {
 
   }
 
+  /**
+   * [item 设置数据源]
+   */
+  public function item($item){
+      $this->data[] = $item;
+      return $this;
+  }
   /**
    * [data 设置数据源]
    * @param  [type] $data [数据对象]
@@ -25,6 +56,32 @@ class Form
   public function data($data){
       $this->data = $data;
       return $this;
+  }
+  /**
+   * [setFormObject 给form Item 列赋值value]
+   * loadAttribute 加载指定对象元素赋值 （最高支持二级对象）
+   */
+  public function itemData($Object){
+      $itemData = $Object->toArray();
+       foreach ($this->data as &$item) {
+              if (!isset($item['value'])) {
+                  @$item['value'] = $itemData[$item['name']];
+              }
+              if (isset($item['loadAttribute'])) {
+                  $loadAttribute = collect($item['loadAttribute'])
+                      ->map(function ($value) {
+                          return explode('.',$value);
+                      });
+                  foreach ($loadAttribute as $key => $value) {
+                      if($key){
+                          @$item[$key] = $itemData[$value[0]][$value[1]];
+                      }else{
+                          @$item['value'] = $itemData[$value[0]][$value[1]];
+                      }
+                  }
+              }
+       }
+       return $this;
   }
   /**
    * [apiUrl 设置 apiUrl API通信网址]
