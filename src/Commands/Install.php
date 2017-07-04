1c75c85e2c1cffb7b107279bea9e0df7bf3b184c
bigrocs <bigrocs@qq.com>
2017-07-04 14:16:50 +0800

    trait to class  add installProviders

--- src/Commands/Install.php
+++ src/Commands/Install.php
@@ -1,21 +1,36 @@
 <?php
 
 namespace CoreCMF\core\Commands;
+use JeroenG\Packager\PackagerHelper;
 
 use Artisan;
 /**
  * trait install
  * @package CoreCMF\core\Commands
  */
-trait Install
+class Install
 {
+    /**
+     * Packager helper class.
+     * @var object
+     */
+    protected $helper;
 
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct(PackagerHelper $helper)
+    {
+        $this->helper = $helper;
+    }
     public function dumpAutoload()
     {
         $this->info('dumpAutoload');
         exec('composer dumpautoload');
     }
-	public function migrate()
+	  public function migrate()
     {
         $this->info('migrate');
         Artisan::call('migrate');
@@ -33,5 +48,14 @@ trait Install
         Artisan::call('db:seed', [
             '--class' => $class
         ]);
+    }
+    public function installProviders($serviceProvider){
+        $appConfigLine = 'App\Providers\RouteServiceProvider::class,';
+        foreach ($serviceProvider as $provider ) {
+            $appConfigLine = $appConfigLine.'
+        '.$provider.',';
+        }
+        dd($this->helper);
+        // $this->helper->replaceAndSave(getcwd().'/config/app.php', 'App\Providers\RouteServiceProvider::class,', $appConfigLine);
     }
 }
