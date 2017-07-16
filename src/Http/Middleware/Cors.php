e88c02465d1eee949633f1af5eb5bd2d55a8918f
bigrocs <bigrocs@qq.com>
2017-07-16 18:58:38 +0800

    8081  8082

--- src/Http/Middleware/Cors.php
+++ src/Http/Middleware/Cors.php
@@ -15,7 +15,7 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $domains = ['http://corecmf.dev','http://localhost:8080'];
+        $domains = ['http://corecmf.dev','http://localhost:8080','http://localhost:8081','http://localhost:8082'];
         if (isset($request->server()['HTTP_ORIGIN'])) {
             $origin = $request->server()['HTTP_ORIGIN'];
             if (in_array($origin, $domains)) {
