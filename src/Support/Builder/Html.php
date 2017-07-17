562e750d7ca361fbae8351966e9563eeb50aa4ba
bigrocs <bigrocs@qq.com>
2017-07-17 13:28:05 +0800

    message

--- src/Support/Builder/Html.php
+++ src/Support/Builder/Html.php
@@ -7,51 +7,61 @@ use CoreCMF\core\Support\Builder\Table;
 
 class Html
 {
-  private $items;
-  private $title;
-  private $config;
-  private $tabs;
+    private $items;
+    private $title;
+    private $config;
+    private $tabs;
+    private $message;
 
-  public function __construct()
-  {
-  }
-  /**
-   * [item html项目]
-   * @param  [type] $item [description]
-   * @return [type]       [description]
-   */
-  public function item($item){
-      $this->items[] = $item->response();
-      return $this;
-  }
-  /**
-   * [item html项目]
-   * @param  [type] $item [description]
-   * @return [type]       [description]
-   */
-  public function config($key,$value){
-      $this->config[$key] = $value;
-      return $this;
-  }
-  /**
-   * [title 设置页面标题]
-   * @param  [type] $title [description]
-   * @return [type]        [description]
-   */
-  public function title($title){
-      $this->title   = $title;
-      return $this;
-  }
-  /**
-   * [response 数据处理返回]
-   * @return   [type]                   [description]
-   */
-  public function response()
-  {
-      $response['title']  = $this->title;
-      $response['items']   = $this->items;
-      $response['config']  = $this->config;
+    public function __construct()
+    {
+    }
+    /**
+     * [item html项目]
+     * @param  [type] $item [description]
+     * @return [type]       [description]
+     */
+    public function item($item){
+        $this->items[] = $item->response();
+        return $this;
+    }
+    /**
+     * [item html项目]
+     * @param  [type] $item [description]
+     * @return [type]       [description]
+     */
+    public function config($key,$value){
+        $this->config[$key] = $value;
+        return $this;
+    }
+    /**
+     * [title 设置页面标题]
+     * @param  [type] $title [description]
+     * @return [type]        [description]
+     */
+    public function title($title){
+        $this->title   = $title;
+        return $this;
+    }
+    /**
+     * [message 提示纤细]
+     * @param  [type] $message [description]
+     */
+    public function message($message){
+        $this->message   = $message;
+        return $this;
+    }
+    /**
+     * [response 数据处理返回]
+     * @return   [type]                   [description]
+     */
+    public function response()
+    {
+        $response['title']  = $this->title;
+        $response['items']   = $this->items;
+        $response['config']  = $this->config;
+        $response['message']  = $this->message;
 
-      return $response;
-  }
+        return $response;
+    }
 }
