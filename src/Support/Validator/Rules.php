9976572dfd0f3f63cf6e3929c5c224bfe9cda171
bigrocs <bigrocs@qq.com>
2017-07-21 14:42:50 +0800

    core 验证去 规则class 验证

---
+++ src/Support/Validator/Rules.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace CoreCMF\core\Support\Validator;
+
+
+class Rules
+{
+    protected $mobile;
+    protected $password;
+    protected $CheckPassword;
+
+    public function mobile(
+      $empty = '请输入手机号码',
+      $error = '请输入正确的手机号码',
+    )
+    {
+
+        return $this->mobile = "
+            (rule, value, callback) => {
+                if (value === '') {
+                  callback(new Error('".$empty."'));
+                } else {
+                    if (!/^1[3578]\d{9}$/.test(value)) {
+                        callback(new Error('".$error."'));
+                    }
+                    callback();
+                }
+            }
+        ";
+    }
+}
