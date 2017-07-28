8aa4fb5ca83c1c8efa86efd04f4dc1fbc20bd03a
bigrocs <bigrocs@qq.com>
2017-07-28 10:02:36 +0800

    add checkPassword password

--- src/Support/Validator/Rules.php
+++ src/Support/Validator/Rules.php
@@ -7,10 +7,12 @@ class Rules
 {
     public $mobile;
     public $password;
-    public $CheckPassword;
+    public $checkPassword;
     public function __construct()
     {
         $this->mobile();
+        $this->password();
+        $this->checkPassword();
     }
     public function mobile(
       $empty = '请输入手机号码',
@@ -20,7 +22,7 @@ class Rules
 
         return $this->mobile = "
             (rule, value, callback) => {
-                if (value === '') {
+                if (value == undefined) {
                   callback(new Error('".$empty."'));
                 } else {
                     if (!/^1[3578]\d{9}$/.test(value)) {
@@ -30,5 +32,44 @@ class Rules
                 }
             }
         ";
+    }
+    public function password(
+      $checkPassword = 'checkPassword',
+      $begin = 6,
+      $end = 16,
+      $empty = '请输入密码'
+    )
+    {
+        return $this->password = "
+            (rule, value, callback) => {
+                if (value == undefined) {
+                  callback(new Error('".$empty."'));
+                } else {
+                    if (!/^.{".$begin.",".$end."}$/.test(value)) {
+                        callback(new Error('密码长度请控制在 ".$begin." 到 ".$end." 个字符'));
+                    }
+                    if (this.fromData['".$checkPassword."'] !== '') {
+                        this.\$refs.bvefrom.validateField('".$checkPassword."');
+                    }
+                  callback();
+                }
+            }";
+    }
+    public function checkPassword(
+      $password = 'password',
+      $empty = '请再次输入密码'
+    )
+    {
+        return $this->checkPassword = "
+            (rule, value, callback) => {
+                if (value == undefined) {
+                  callback(new Error('".$empty."'));
+                }
+                if (value !== this.fromData['".$password."']) {
+                  callback(new Error('两次输入密码不一致!'));
+                } else {
+                  callback();
+                }
+            }";
     }
 }
