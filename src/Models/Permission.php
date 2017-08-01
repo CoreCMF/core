c7ab1d04d6c9193f0506b16a0d20a71e914ea573
bigrocs <bigrocs@qq.com>
2017-08-01 11:48:57 +0800

    add isExist

--- src/Models/Permission.php
+++ src/Models/Permission.php
@@ -6,6 +6,11 @@ use Zizaco\Entrust\EntrustPermission;
 class Permission extends EntrustPermission
 {
     public $table = 'entrust_roles';
-    
+
     protected $fillable = ['name', 'display_name', 'description'];
+
+    public function isExist($name)
+    {
+        return ($this->where('name', $name)->count() != 0) ? true : false;
+    }
 }
