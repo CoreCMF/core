301aab8beb9794078d47601ed7f4ca1b3c2ce4a4
bigrocs <bigrocs@qq.com>
2017-08-27 17:19:51 +0800

    permissionId

--- src/Models/Role.php
+++ src/Models/Role.php
@@ -7,5 +7,14 @@ class Role extends EntrustRole
 {
 
     protected $fillable = ['name', 'display_name', 'description', 'group'];
+    /**
+     * 关联权限id
+     */
+    public function permissionId($id){
+        $roles = $this->find($id)->load('perms');//关联权限数据
+        return $roles->perms->map(function ($item, $key) {
+            return $item->id;
+        });
+    }
 
 }
