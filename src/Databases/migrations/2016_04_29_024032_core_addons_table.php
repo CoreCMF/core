f72a05c53e076e80e54c8759d7422b79bcb04b63
bigrocs <bigrocs@qq.com>
2017-09-06 10:31:01 +0800

    改名

--- src/Databases/migrations/2016_04_29_024032_core_addons_table.php
+++ src/Databases/migrations/2016_04_29_024032_core_addons_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AdminAddonsTable extends Migration
+class CoreAddonsTable extends Migration
 {
     /**
      * Run the migrations.
