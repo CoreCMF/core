e9cb06a12750179b2ad235dedc5a9611b87ec39d
bigrocs <bigrocs@qq.com>
2018-01-03 16:19:36 +0800

    自动通过composer获取包配置

--- src/Support/Package/Manage.php
+++ src/Support/Package/Manage.php
@@ -5,22 +5,60 @@ namespace CoreCMF\Core\Support\Package;
 use Artisan;
 use CoreCMF\Core\App\Models\Package;
 use CoreCMF\Core\Support\Package\Psr4;
+use Illuminate\Filesystem\Filesystem;
 
 class Manage
 {
     protected $psr4;
     protected $packageModel;
-    public function __construct(Psr4 $psr4,Package $packageRepo)
+    protected $files;
+    public function __construct(Psr4 $psr4, Package $packageRepo, Filesystem $files)
     {
         $this->psr4 = $psr4;
         $this->packageModel = $packageRepo;
+        $this->files = $files;
+    }
+    /**
+     * [updatePackage 更新数据库包]
+     * @return   [type]         [description]
+     * @Author   bigrocs
+     * @QQ       532388887
+     * @Email    bigrocs@qq.com
+     * @DateTime 2018-01-03
+     */
+    public function updatePackage()
+    {
+        $packages = $this->getPackage();
+        print_R($packages);
+    }
+    /**
+     * [getPackage 获取corecmf包扩展信息]
+     * @return   [type]         [description]
+     * @Author   bigrocs
+     * @QQ       532388887
+     * @Email    bigrocs@qq.com
+     * @DateTime 2018-01-03
+     */
+    public function getPackage()
+    {
+        $packages = [];
+        if ($this->files->exists($path = app()->basePath().'/vendor/composer/installed.json')) {
+            $packages = json_decode($this->files->get($path), true);
+        }
+        return collect($packages)->mapWithKeys(function ($package) {
+            return [$this->format($package['name']) => $package['extra']['corecmf'] ?? []];
+        })->filter()->all();
     }
 
-    public function namespaceDir($namespace)
+    /**
+     * Format the given package name.
+     *
+     * @param  string  $package
+     * @return string
+     */
+    protected function format($package)
     {
-        $psr4 = require base_path().'/vendor/composer/autoload_psr4.php';
-        $namespace = str_replace('\\\\',"\\",$namespace.'\\');//防止忘记加／
-        return array_key_exists($namespace,$psr4)? $psr4[$namespace][0]: false;
+        return str_replace(app()->basePath().'/', '', $package);
     }
     /**
      * [install 通过配置信息安装模块]
@@ -39,13 +77,13 @@ class Manage
                           'message'   => '模块安装成功.',
                           'type'      => 'success',
                       ];
-            }else{
+            } else {
                 return [
                           'message'   => '模块插件已存在请勿重复安装.',
                           'type'      => 'warning',
                       ];
             }
-        }else{
+        } else {
             return [
                       'message'   => $check,
                       'type'      => 'warning',
@@ -69,13 +107,13 @@ class Manage
             if (is_file($configDir)) {
                 $config = include $configDir;
                 return $this->install($config);
-            }else{
+            } else {
                 return [
                             'message'   => '未能找到插件配置文件!请确认您的插件包是否正确。',
                             'type'      => 'error',
                         ];
             }
-        }else{
+        } else {
             return [
                         'message'   => '未能找到插件命名空间!请确认您输入的命名空间是否正确。',
                         'type'      => 'error',
@@ -93,14 +131,13 @@ class Manage
         $message = '配置文件缺少下面参数';
         $keys = ['name','title','description','author','version','serviceProvider','install','uninstall'];
         foreach ($keys as $key) {
-            if (array_key_exists($key,$config)) {
+            if (array_key_exists($key, $config)) {
                 $status = true;
-            }else{
+            } else {
                 $message .= ' \''.$key.'\'';
                 $status = false;
             }
         }
         return $status? true: $message;
     }
-
 }
