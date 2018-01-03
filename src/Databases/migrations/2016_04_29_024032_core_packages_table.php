85a3082e0fb3adf0be2689a2f1b8fdc2b280c908
bigrocs <bigrocs@qq.com>
2018-01-03 16:19:09 +0800

    serviceProvider to provider

--- src/Databases/migrations/2016_04_29_024032_core_packages_table.php
+++ src/Databases/migrations/2016_04_29_024032_core_packages_table.php
@@ -15,13 +15,13 @@ class CorePackagesTable extends Migration
     {
         Schema::create('core_packages', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name',64)             ->comment('标识')->unique();
-            $table->string('title',64)            ->comment('名称');
+            $table->string('name', 64)             ->comment('标识')->unique();
+            $table->string('title', 64)            ->comment('名称');
             $table->text('description')           ->comment('描述')->nullable();
-            $table->string('author',32)           ->comment('作者')->nullable();
-            $table->string('version',8)           ->comment('版本')->nullable();
-            $table->string('serviceProvider',128) ->comment('服务提供者')->nullable();
-            $table->string('uninstall',64)       ->comment('卸载artisan')->nullable();
+            $table->string('author', 32)           ->comment('作者')->nullable();
+            $table->string('version', 8)           ->comment('版本')->nullable();
+            $table->string('provider', 255)        ->comment('服务提供者')->nullable();
+            $table->string('uninstall', 64)        ->comment('卸载artisan')->nullable();
             $table->tinyInteger('status')         ->comment('状态')->default(1);
             $table->bigInteger('sort')            ->comment('排序')->unsigned()->default(0);
             $table->timestamps();
