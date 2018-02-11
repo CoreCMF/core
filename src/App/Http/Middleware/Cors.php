8e4f5a2978758d4473cc10cf6fc3c3196b682bed
bigrocs <bigrocs@qq.com>
2018-02-11 08:01:52 +0800

    test

--- src/App/Http/Middleware/Cors.php
+++ src/App/Http/Middleware/Cors.php
@@ -15,12 +15,12 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        $domains = ['http://corecmf.dev','http://localhost:8080','http://localhost:8081','http://localhost:8082'];
+        $domains = ['http://corecmf.test','http://localhost:8080','http://localhost:8081','http://localhost:8082'];
         if (isset($request->server()['HTTP_ORIGIN'])) {
             $origin = $request->server()['HTTP_ORIGIN'];
             if (in_array($origin, $domains)) {
                 header('Access-Control-Allow-Origin: '.$origin);
-                header('Access-Control-Allow-Credentials: true' );
+                header('Access-Control-Allow-Credentials: true');
                 header('Access-Control-Allow-Headers: Origin, Content-Type, Authorization,X-Requested-With,X-CSRF-TOKEN,X-XSRF-TOKEN,XMLHttpRequest');
             }
         }
