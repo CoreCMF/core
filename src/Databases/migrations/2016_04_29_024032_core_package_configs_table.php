54bffc16705d9c87348a10873256891beb4c48d3
bigrocs <bigrocs@bigrocsdeiMac.local>
2018-03-15 21:49:27 +0800

    取消唯一

--- src/Databases/migrations/2016_04_29_024032_core_package_configs_table.php
+++ src/Databases/migrations/2016_04_29_024032_core_package_configs_table.php
@@ -15,7 +15,7 @@ class CorePackageConfigsTable extends Migration
     {
         Schema::create('core_package_configs', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name', 64)             ->comment('扩展包名称')->unique();
+            $table->string('name', 64)             ->comment('扩展包名称');
             $table->string('key', 128)             ->comment('配置键值');
             $table->json('value')                  ->comment('配置内容')->nullable();
             $table->string('status', 16)           ->comment('状态')->default('close');
